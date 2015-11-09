b531d6c42147b1699166ed8424eb0930b8232101
Ivica Stojanoski <s.ivica@gmail.com>
2015-11-09 20:45:55 +0100

    Added RecyclerView

--- app/src/main/java/com/stojanoski/ivica/assignment1android/MainActivity.java
+++ app/src/main/java/com/stojanoski/ivica/assignment1android/MainActivity.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mCitiesRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private TextView mEmptyView;
+    private ArrayList<String> mCities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mCitiesRecyclerView = (RecyclerView) findViewById(R.id.cities_recycler_view);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mCitiesRecyclerView.setLayoutManager(mLayoutManager);
+
+        mEmptyView = (TextView) findViewById(R.id.empty_view);
+        mCities = new ArrayList<>();
+
+        checkIfEmpty();
+    }
+
+    private void checkIfEmpty() {
+        if (mCities.size() == 0) {
+            mCitiesRecyclerView.setVisibility(View.GONE);
+            mEmptyView.setVisibility(View.VISIBLE);
+        } else {
+            mCitiesRecyclerView.setVisibility(View.VISIBLE);
+            mEmptyView.setVisibility(View.GONE);
+        }
     }
 
     @Override
